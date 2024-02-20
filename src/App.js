26adfec93745ce93f9fdca3e29eb36635c1c4d2b
RosieOh <dhxogns920@gmail.com>
2024-02-20 13:38:45 +0900

    🔭feat : useEffect를 활용한 타입 별 로그🔭

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
   const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1);
   const onChange =  (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
   useEffect(() => {
-    console.log("Call the Api...")
+    console.log("I run only once.");
   }, []);
-  console.log("SEARCH FOR", keyword);
+  useEffect(() => {
+      console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+}, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword && counter change");
+  }, [keyword, counter]);
+  
   return (
     <div>
       <input
